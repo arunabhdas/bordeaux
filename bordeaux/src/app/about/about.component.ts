80d1ac509c74fb33441c5f78bc085f9427da2a92
Arunabh Das <arunabhdas@gmail.com>
2019-03-28 12:34:43 -0400

    Added contact form

--- bordeaux/src/app/about/about.component.ts
+++ bordeaux/src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  singleUnknownResponse: Object;
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
+    this.dataService.getSingleUnknownResponse().subscribe(data => {
+      this.singleUnknownResponse = data[0];
+      console.log(this.singleUnknownResponse);
+    }) 
   }
 
 }
